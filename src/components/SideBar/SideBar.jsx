aa3cd0ca8982142566a0fba851832c5cb968a5ab
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-04-29 01:51:18 +0300

    first commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -47,7 +47,7 @@ export default function SideBar() {
             {/* <h3 className="px-4 py-2 text-sm font-medium text-gray-500">Navigation</h3> */}
             {links.map((link, index) => (
               <NavLink
-                to={link.url}
+                to={link.path}
                 key={index}
                 className={({ isActive }) =>
                   `flex items-center justify-between py-3 px-4 cursor-pointer border-b border-gray-100 
